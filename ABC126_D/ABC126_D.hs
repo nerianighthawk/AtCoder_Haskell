91c3e7450d30012286e16d75b4dc21c8f0c8c4dd
nerianighthawk <hiroya.nakakawaji@gmail.com>
2020-02-03 13:46:48 +0900

    fix ABC126 C, D

---
+++ ABC126_D/ABC126_D.hs
@@ -0,0 +1,22 @@
+toInt :: (String -> Int)
+toInt x = read x ::Int
+
+getInts = do
+    x <- getLine
+    let y = (map$toInt)$words x
+    return y
+
+a :: Int -> Int
+a x = 2^x
+
+ret :: [Int] -> Int -> Int -> Int
+ret hs c h
+    | head hs > h = (head hs) - 1
+    | otherwise = ret (tail hs) (c + 1) h
+
+main = do
+    ns <- getInts
+    let h = head ns
+    let r = ret (iterate a 1) 0 h
+    putStrLn $ show r
+    
